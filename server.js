a3942a0ebf1c6e19ec5861a2ece976fe9bdbd21b
Abdul Rehman <s.abderrehman@gmail.com>
2021-06-21 15:07:22 +0500

    Return response

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const http = require('http');
 const express = require('express');
+
 const app = express();
-const server = http.createServer(app);
-app.use((req, res,next)=>{
-  console.log('In the Middleware!')
-  next()
-})
-app.use((req, res,next)=>{
-  console.log('In the another Middleware!')
-})
-server.listen(9000);
+app.use(express.urlencoded({ extended: false }));
+app.use('/', (req, res, next) => {
+  console.log('This is always RUN!!!!');
+  next();
+});
+app.use('/addUser', (req, res, next) => {
+  res.send('<form action="/users" method="POST" ><input type="text" name="title"/><button type="submit">Submit</button></form>');
+});
+app.use('/users', (req, res, next) => {
+  console.log(req.body);
+  res.send('<h1>Hello From User Page</h1>');
+});
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello From Expresss</h1>');
+  next();
+});
+
+app.listen(9000);
