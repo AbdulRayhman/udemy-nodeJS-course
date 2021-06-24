24c2f975758358264fa8555025e85d31b05f10e8
Abdul Rehman <s.abderrehman@gmail.com>
2021-06-25 02:08:32 +0500

    Adding Styling and Serving CSS

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
